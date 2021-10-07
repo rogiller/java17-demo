f7d5478e97a8063f6f679bd57aedf359616578ce
Roger Diller <roger@flexrentalsolutions.com>
2021-10-07 15:27:59 -0400

    initial commit

---
+++ src/main/java/com/poc/java17demo/Java17DemoApplication.java
@@ -0,0 +1,13 @@
+package com.poc.java17demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java17DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java17DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/java17demo/Java17DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.java17demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java17DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
